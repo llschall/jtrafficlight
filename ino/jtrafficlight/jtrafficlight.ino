a92211c03e31b2feb894ffccdb36f405c508210a
schall0 <schall@gmx.fr>
2023-09-28 23:36:20 +0200

    [Light] Experiment blinking intervals

--- ino/jtrafficlight/jtrafficlight.ino
+++ ino/jtrafficlight/jtrafficlight.ino
@@ -1,7 +1,51 @@
+
+uint8_t led_r = 7;
+uint8_t led_y = 8;
+uint8_t led_g = 9;
+
 void setup() {
-  // Work in Progress !
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led_r, OUTPUT);
+  pinMode(led_y, OUTPUT);
+  pinMode(led_g, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(led_r, LOW);
+  digitalWrite(led_y, LOW);
+  digitalWrite(led_g, LOW);
 }
 
+int i = 0;
+
 void loop() {
-  delay(1000);
+
+  if (i == 64) {
+    i = 0;
+  }
+
+  if (Serial.available() > 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  digitalWrite(led_r, switchOn(1) ? HIGH : LOW);
+  digitalWrite(led_y, switchOn(64) ? HIGH : LOW);
+  digitalWrite(led_g, switchOn(32) ? HIGH : LOW);
+
+  delay(64);
+  i++;
+}
+
+bool switchOn(int d) {
+  bool on = false;
+
+  for (int j = 0; j <= i; j++) {
+    if (j % d == 0) {
+      on = !on;
+    }
+  }
+  return on;
 }
