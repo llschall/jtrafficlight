6a7f171d3dd861ab424f41ef515a6a32d5fcecbf
llschall <llschall@gmx.com>
2023-09-30 21:30:47 +0200

    [Test] Add TrafficLightTest

---
+++ src/test/java/TrafficLightTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TrafficLightTest {
+
+    @Test
+    public void testTrue() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void testFalse() {
+        Assertions.assertTrue(false);
+    }
+
+}
